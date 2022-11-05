97aeadef766108f3168242983be23a621f40d0f4
uwuSk1 <orionvt3@gmail.com>
2022-11-05 16:38:42 +0200

    Added Questions Core

---
+++ Quizle/Quizle.Core.Questions/Contracts/IAPIService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizle.Core.Questions.Contracts
+{
+    public interface IAPIService
+    {
+        Task<T> GetDataAsync<T>(string url);
+    }
+}
---
+++ Quizle/Quizle.Core.Questions/Services/APIService.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Quizle.Core.Questions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizle.Core.Questions.Services
+{
+    public class APIService : IAPIService
+    {
+        public async Task<T> GetDataAsync<T>(string url)
+        {
+            var json = await new WebClient().DownloadStringTaskAsync(url);
+            if (string.IsNullOrEmpty(json))
+            {
+                Console.WriteLine("Json is empty");
+            }
+            var triviaObj = JsonConvert.DeserializeObject<T>(json);           
+            return triviaObj;
+        }
+    }
+}
